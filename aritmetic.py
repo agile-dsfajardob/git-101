c9ab13a99548fed522bea9404838a72d29f40a22
dsfajardob <dsfajardob@gmail.com>
2023-12-12 17:12:13 -0500

    add the multiply two numbers inside aritmetic

--- aritmetic.py
+++ aritmetic.py
@@ -3,3 +3,6 @@ def sum_two_numbers(num_1: int, num_2: int) -> int:
 
 def substract_two_numbers(num_1: int, num_2: int) -> int:
 	return num_1 - num_2
+
+def multiply_two_numbers(num_1: int, num_2: int) -> int:
+	return num_1 * num_2
